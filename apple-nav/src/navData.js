63740ebafc06be03fa9cefcfecb32ea98565d0d6
Josemaria <josemarianriagu25@gmail.com>
2019-06-12 11:36:42 +0800

    Define navData

---
+++ apple-nav/src/navData.js
@@ -0,0 +1,279 @@
+export const navData = [{
+    id: '1',
+    name: 'Mac',
+    url: "mac",
+    subnav: [{
+        name: 'MacBook',
+        thumbnail: '#',
+        url: "macbook"
+    },
+    {
+        name: 'MacBook Air',
+        thumbnail: '#',
+        url: "macbook-air"
+    },
+    {
+        name: 'MacBook Pro',
+        thumbnail: '#',
+        url: "macbook-pro"
+    },
+    {
+        name: 'iMac',
+        thumbnail: '#',
+        url: "imac"
+    },
+    {
+        name: 'iMac Pro',
+        thumbnail: '#',
+        url: "imac-pro"
+    },
+    {
+        name: 'Mac Pro',
+        thumbnail: '#',
+        url: "mac-pro"
+    },
+    {
+        name: 'Mac Mini',
+        thumbnail: '#',
+        url: "mac-mini"
+    },
+    {
+        name: 'Compare',
+        thumbnail: '#',
+        url: "compare"
+    },
+    {
+        name: 'Pro Display XDR',
+        thumbnail: '#',
+        url: "pro-display-xdr"
+    },
+    {
+        name: 'Accessories',
+        thumbnail: '#',
+        url: "accessories"
+    },
+    {
+        name: 'Mojave',
+        thumbnail: '#',
+        url: "mojave"
+    }]
+},
+{
+    id: '2',
+    name: 'iPad',
+    url: "ipad",
+    subnav: [{
+        name: 'iPad Pro',
+        thumbnail: '#',
+        url: "ipad-pro"
+    },
+    {
+        name: 'iPad Air',
+        thumbnail: '#',
+        url: "ipad-air"
+    },
+    {
+        name: 'iPad',
+        thumbnail: '#',
+        url: "ipad"
+    },
+    {
+        name: 'iPad Mini',
+        thumbnail: '#',
+        url: "ipad-mini"
+    },
+    {
+        name: 'Compare',
+        thumbnail: '#',
+        url: "compare"
+    },
+    {
+        name: 'Apple Pencil',
+        thumbnail: '#',
+        url: "apple-pencil"
+    },
+    {
+        name: 'Smart Keyboard',
+        thumbnail: '#',
+        url: "smart-keyboard"
+    },
+    {
+        name: 'Accessories',
+        thumbnail: '#',
+        url: "accessories"
+    },
+    {
+        name: 'iOS 12',
+        thumbnail: '#',
+        url: "ios-12"
+    }]
+},
+{
+    id: '3',
+    name: 'iPhone',
+    url: "iphone",
+    subnav: [{
+        name: 'iPhone Xs',
+        thumbnail: '#',
+        url: "iphone-xs"
+    },
+    {
+        name: 'iPhone Xr',
+        thumbnail: '#',
+        url: "iphone-xr"
+    },
+    {
+        name: 'iPhone 8',
+        thumbnail: '#',
+        url: "iphone-8"
+    },
+    {
+        name: 'iPhone 7',
+        thumbnail: '#',
+        url: "iphone-7"
+    },
+    {
+        name: 'Compare',
+        thumbnail: '#',
+        url: "compare"
+    },
+    {
+        name: 'Apple Card',
+        thumbnail: '#',
+        url: "apple-card"
+    },
+    {
+        name: 'AirPods',
+        thumbnail: '#',
+        url: "airpods"
+    },
+    {
+        name: 'Accessories',
+        thumbnail: '#',
+        url: "accessories"
+    },
+    {
+        name: 'iOS 12',
+        thumbnail: '#',
+        url: "ios-12"
+    }]
+},
+{
+    id: '4',
+    name: 'Watch',
+    url: "watch",
+    subnav: [{
+        name: 'Apple Watch Series 4',
+        thumbnail: '#',
+        url: "apple-watch-series-4"
+    },
+    {
+        name: 'Apple Watch Nike+',
+        thumbnail: '#',
+        url: "apple-watch-nike+"
+    },
+    {
+        name: 'Apple Watch Hermes',
+        thumbnail: '#'
+    },
+    {
+        name: 'Apple Watch Series 3',
+        thumbnail: '#',
+        url: "apple-watch-series-3"
+    },
+    {
+        name: 'WatchOS',
+        thumbnail: '#',
+        url: "watchos"
+    },
+    {
+        name: 'Bands',
+        thumbnail: '#',
+        url: "bands"
+    },
+    {
+        name: 'Accessories',
+        thumbnail: '#',
+        url: "accessories"
+    },
+    {
+        name: 'Compare',
+        thumbnail: '#',
+        url: "compare"
+    }]
+},
+{
+    id: '5',
+    name: 'TV',
+    url: "tv",
+    subnav: [{
+        name: 'Apple TV app',
+        thumbnail: '#',
+        url: "apple-watch-app"
+    },
+    {
+        name: 'Apple TV+',
+        thumbnail: '#',
+        url: "apple-tv+"
+    },
+    {
+        name: 'Apple TV 4K',
+        thumbnail: '#',
+        url: "apple-tv-4k"
+    },
+    {
+        name: 'Apple TV HD',
+        thumbnail: '#',
+        url: "apple-tv-hd"
+    },
+    {
+        name: 'Airplay',
+        thumbnail: '#',
+        url: "airplay"
+    },
+    {
+        name: 'Accessories',
+        thumbnail: '#',
+        url: "accessories"
+    }]
+},
+{
+    id: '6',
+    name: 'Music',
+    url: "music",
+    subnav: [{
+        name: 'Apple Music',
+        thumbnail: '#',
+        url: "apple-music"
+    },
+    {
+        name: 'iTunes',
+        thumbnail: '#',
+        url: "itunes"
+    },
+    {
+        name: 'HomePod',
+        thumbnail: '#',
+        url: "homepod"
+    },
+    {
+        name: 'AirPods',
+        thumbnail: '#',
+        url: "airpods"
+    },
+    {
+        name: 'iPod touch',
+        thumbnail: '#',
+        url: "ipod-touch"
+    },
+    {
+        name: 'Music Accessories',
+        thumbnail: '#',
+        url: "accessories"
+    },
+    {
+        name: 'Gift Cards',
+        thumbnail: '#',
+        url: "gift-cards"
+    }]
+}]
